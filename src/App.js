678b048b9dc82a48481d92cc357a8e9cf940b9a0
Andrew Williamson <acwilliamson419@gmail.com>
2019-07-10 12:00:17 -0700

    Imports Semantic-UI-React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {menu} from 'semantic-ui-react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="ui menu">
+          <a class="active item">Editorials</a>
+          <a class="item">Reviews</a>
+          <a class="item">Upcoming Events</a>
+        </div>
       </header>
     </div>
   );
