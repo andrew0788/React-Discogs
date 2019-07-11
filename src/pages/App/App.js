6c69e3d6f000ad3ef9cb5195fd2ad7664bb88c1e
Andrew Williamson <acwilliamson419@gmail.com>
2019-07-10 22:31:29 -0700

    Adds route to sign up page

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom'
 import './App.css';
-import MenuBar from '../../components/MenuBar';
-
+import MenuBar from '../../components/MenuBar/MenuBar';
+import SignUpPage from '../SignUpPage/SignUpPage';
+import LoginPage from '../LoginPage/LoginPage';
+import userService from '../../utils/userService';
 
 
 const handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
 
-function App() {
-  return (
-    <MenuBar />
-  );
+class App extends Component {
+
+
+  handleSingupOrLogin = () => {
+    this.setState({user: userService.getUser()});
+  }
+
+
+  render(){
+    return(
+      <div>
+        <header>
+          <MenuBar />
+        </header>
+
+        <Switch>
+          <Route exact path='/signup' render={({ history }) =>
+            <SignUpPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          }/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
